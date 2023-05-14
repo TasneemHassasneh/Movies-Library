1fcb93e64f87308a0fb281163654f09625b48104
TasneemHassasneh <tasneemhassasneh890@gmail.com>
2023-05-14 12:13:19 +0300

    Add routes and error handling

--- README.md
+++ README.md
@@ -7,25 +7,17 @@
 ![WRRC-Img](WRRC%20.png)
 
 ## Overview
+Movies-Library is a simple Node.js web application that provides a list of movies and favorite page. It utilizes The Movie DB API to fetch movie data and supports various routes for retrieving trending movies, searching movies, discovering movies based on criteria, and retrieving detailed information about specific movies.
 
 ## Getting Started
-
 To build this app on your own machine and get it running, follow these steps:
-- Create a new repository on GitHub called "Movies-Library".
-- Clone the repository to your local machine using the command `git clone *<repository url>*.`
-- Navigate to the project directory and run `npm init -y` to - initialize the project with default settings.
-- Create the basic file structure for the project by creating the following files:
-  1. **server.js:** *This file will contain your server-side code.*
-  2. **.gitignore:** *This file will specify which files should be ignored by Git.*
-  3. **.eslintrc.json:** *This file will contain your ESLint configuration.*
-- Install the required packages by running the command `npm install express cors`.
-- Build the following routes using the *GET* request:
-  1. **Home Page Endpoint: "/"**
-     - Create a route with a method of *GET* and a *path of "/".*
-     - The callback should use the provided JSON data.
-     - Create a **constructor function** to ensure your data follows the same format.
-  2. **Favorite Page Endpoint: "/favorite"**
-     - Create a route with a method of *GET* and a *path of "/favorite".*
-     - The callback should respond with the message **"Welcome to Favorite Page".**
-- Create a function to handle server errors with a **status of *500* and a response message of *"Sorry, something went wrong"* .**
-- Create a function to handle ***"page not found"* errors with a status of *404*.***
+
+1. Clone the repository to your local machine.
+2. Install the required dependencies by running `npm install`.
+3. Obtain an API key from The Movie DB API (https://www.themoviedb.org/) by creating an account and following their documentation.
+4. Create a `.env` file in the project directory and add the following line, replacing `YOUR_API_KEY` with your actual API key:
+5. Update the `.gitignore` file to include `.env` so that your API key is not exposed in your repository.
+6. Start the server by running `npm start`.
+7. Open your web browser and navigate to `http://localhost:3000` to access the Movies-Library application.
+
+
--- server.js
+++ server.js
@@ -4,33 +4,115 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const data = require('./Movie-Data/data.json');
+const axios = require('axios');
+
+
+require('dotenv').config();
 
 app.use(cors());
 
-// Constructor function to ensure data follows the same format
-function Movie(title, poster_path, overview) {
-    this.title = title;
-    this.poster_path = poster_path;
-    this.overview = overview;
-    Movie.allMovie.push(this);
-  }
-  Movie.allMovie=[];
+
 
 // Home Page Endpoint
 app.get('/', (req, res) => {
+    // Constructor function to ensure data follows the same format
+    function Movie(title, poster_path, overview) {
+        this.title = title;
+        this.poster_path = poster_path;
+        this.overview = overview;
+    }
     // Sample movie data
-    const movie = new Movie(
-      req.body.title,req.body.poster_path,req.body.overview
-    );
+    const movie = new Movie(data.title,data.poster_path,data.overview);
     // Send the movie data as the response
     res.send(movie);
   });
 
 // Favorite Page Endpoint: /favorite
-app.get('/favorite', (req, res) => {
-  res.send("Welcome to Favorite Page");
-});
+app.get('/favorite', (req, res) => {res.send("Welcome to Favorite Page");});
 
+// Trending Endpoint
+app.get('/trending', async (req, res) => {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`);
+  
+      // Extract relevant data from the API response
+      const movies = response.data.results.map((movie) => ({
+        id: movie.id,
+        title: movie.title,
+        release_date: movie.release_date,
+        poster_path: movie.poster_path,
+        overview: movie.overview,
+      }));
+  
+      res.send(movies);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({status: 500,responseText: 'Sorry, something went wrong',});
+    }
+  });
+  
+  // Search Endpoint
+  app.get('/search', async (req, res) => {
+    const { query } = req.query;
+  
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${query}`
+      );
+  
+      // Extract relevant data from the API response
+      const movies = response.data.results.map((movie) => ({
+        id: movie.id,
+        title: movie.title,
+        release_date: movie.release_date,
+        poster_path: movie.poster_path,
+        overview: movie.overview,
+      }));
+  
+      res.send(movies);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        status: 500,
+        responseText: 'Sorry, something went wrong',
+      });
+    }
+  });
+
+  app.get('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`
+      );
+  
+      // Extract relevant data from the API response
+      const movie = {
+        id: response.data.id,
+        title: response.data.title,
+        release_date: response.data.release_date,
+        poster_path: response.data.poster_path,
+        overview: response.data.overview,
+        runtime: response.data.runtime,
+        genres: response.data.genres.map((genre) => genre.name),
+        production_companies: response.data.production_companies.map(
+          (company) => company.name
+        ),
+      };
+  
+      res.send(movie);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        status: 500,
+        responseText: 'Sorry, something went wrong',
+      });
+    }
+  });
+  
+  
+  
 // Error handling for server error (status 500)
 app.use((err, req, res, next) => {
   res.status(500).json({
